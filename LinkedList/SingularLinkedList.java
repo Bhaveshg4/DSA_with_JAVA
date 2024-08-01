7a7c0d1e7f28dc51c0c7c16ed46e4a490debaa36
Bhaveshg4 <118620727+Bhaveshg4@users.noreply.github.com>
2024-08-02 00:53:46 +0530

    LinkedList size and deletion

--- LinkedList/SingularLinkedList.java
+++ LinkedList/SingularLinkedList.java
@@ -2,6 +2,11 @@ package LinkedList;
 
 public class SingularLinkedList {
     Node head;
+    private int size;   
+
+    SingularLinkedList(){
+        this.size=0;
+    }
 
     class Node {
         String data;
@@ -10,6 +15,7 @@ public class SingularLinkedList {
         Node(String data) {
             this.data = data;
             this.next = null;
+            size++;
         }
     }
 
@@ -51,12 +57,53 @@ public class SingularLinkedList {
         }
         System.out.println("NULL");
     }
+    //delete first
+    public void DeleteFirst(){
+        if(head==null){
+            System.out.println("The list is empty");
+            return;
+        }
+        size--;
+        head = head.next;
+    }
+    //delete last
+    public void deleteLast(){
+        if(head==null){
+            System.out.println("The list is empty");
+            return;
+        }
+        size--;
+        if (head.next==null) {
+            head=null;
+            return;
+            
+        }
+        Node secondLast = head;
+        Node lastNode = head.next; //if head.next = null -> lastNode = null
+        while (lastNode.next!=null) {//null.next ->error
+            lastNode=lastNode.next;
+            secondLast = secondLast.next;
+            
+        }
+        secondLast.next=null;
+
+    }
+    public int getSize(){
+        return size;
+    }
 
     public static void main(String args[]) {
         SingularLinkedList list = new SingularLinkedList();
         list.addFirst("a");
         list.addFirst("b");
         list.addLast("is");
+        list.addLast("list");
+        list.addLast("yash");
+
+
         list.printList();
+        list.deleteLast();
+        list.DeleteFirst();
+        System.out.println(list.getSize());
     }
 }
