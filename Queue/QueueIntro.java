8b72989fb952e831aa7507f184256e07c92671ba
Bhaveshg4 <118620727+Bhaveshg4@users.noreply.github.com>
2024-08-03 01:16:36 +0530

    Queue

---
+++ Queue/QueueIntro.java
@@ -0,0 +1,12 @@
+package Queue;
+/* ____________________
+ * |______|_____|_____|
+ * Front part          Rear Part
+ * Queue mai rear se element add karenge, and jo pehle jayega woh pehle bahar niklega (FIFO)
+ * Enque (Add operation), Dequeue(Remove operation), 
+ * Front(peek operation: giving the 1st element of queue), Deque(double ended queue)
+ * -1 shows the queue is full empty
+ */
+public class QueueIntro {
+
+}
